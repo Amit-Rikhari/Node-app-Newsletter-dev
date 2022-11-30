9d061da339fa70011d6a7e99fd370891b432284f
Amit-Rikhari <116537890+Amit-Rikhari@users.noreply.github.com>
2022-11-30 10:52:38 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -4,8 +4,7 @@ sonarqubeScanner({
   serverUrl: 'http://172.31.28.19:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
-	    'sonar.projectName': 'Node JS Application - Sample',
-	    'sonar.projectKey':'NodeJsMithunTechnologies',
+	    'sonar.projectName': 'Node JS Application - Sample'
 	    //'sonar.login': 'squ_7d24dd1e8ab896e1048111eefabd496b15c667e0',
 	    'sonar.login': 'admin',
 	    'sonar.password': '1234',
